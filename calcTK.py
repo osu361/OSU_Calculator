e84d885683a20581e4ae57fe78ddd9419a70417a
David <daviddvega@gmail.com>
2020-02-01 23:54:54 -0600

    reorganized buttons

--- calcTK.py
+++ calcTK.py
@@ -1,6 +1,6 @@
 from tkinter import *
-import re
 import tkinter.font as font
+import re
 
 
 class Calculator:
@@ -9,84 +9,87 @@ class Calculator:
         self.master = master
         master.title("OSU Calculator")
 
-        # fonts
+        # screen font
         self.screenFont = font.Font(weight="bold", size=40)
+
+        # button font
         self.myFont = font.Font(weight="bold", size=20)
 
         # create screen widget
-        self.screen = Text(master, state="disabled", width=20, height=2,
+        self.screen = Text(master, state="disabled", width=20, height=1,
                            background="#FF7F50", foreground="black",
                            font=self.screenFont,)
 
         # position screen in window
-        self.screen.grid(row=0, column=0, columnspan=5, padx=15, pady=15)
+        self.screen.grid(row=0, column=0, columnspan=6, padx=10, pady=20)
 
         # initialize screen value as empty
         self.equation = ""
 
-        # create buttons using method createButton
-        b1 = self.createButton(7)
-        b2 = self.createButton(8)
-        b3 = self.createButton(9)
-        b4 = self.createButton(u"clear", None)
+        # first row buttons
+        b1 = self.createButton(u"clear", None)
+        b2 = self.createButton('%')
+        b3 = self.createButton(u"\u232B", None)  # not working yet
+        b4 = self.createButton("/")
 
-        # Dropdown menu
-        self.options = [
-            "Save History",
-            "Save to File",
-            "Calculation Log"
-        ]
+        # Dropdown menu first row
+        self.options = ["Save History", "Save to File",
+                        "Calculation Log", "Hamming Code"]
         variable = StringVar(master)
         variable.set(self.options[0])  # default value
-        b4a = OptionMenu(master, variable, *self.options)
-        b4a.config(font=self.myFont)
+        b5 = OptionMenu(master, variable, *self.options)
+        b5.config(font=self.myFont)
 
-        b5 = self.createButton(4)
-        b6 = self.createButton(5)
-        b7 = self.createButton(6)
-        b8 = self.createButton("/")
-        b8a = self.createButton("ENTER")
+        # 2nd row buttons
+        b6 = self.createButton(7)
+        b7 = self.createButton(8)
+        b8 = self.createButton(9)
+        b9 = self.createButton("*")
+        b10 = self.createButton("Enter", None)
 
-        b9 = self.createButton(1)
-        b10 = self.createButton(2)
-        b11 = self.createButton(3)
-        b12 = self.createButton("*")
-        b12a = None  # place holder for button
+        # 3rd row buttons
+        b11 = self.createButton(4)
+        b12 = self.createButton(5)
+        b13 = self.createButton(6)
+        b14 = self.createButton("-")
+        b15 = None  # place holder for button
 
-        b13 = self.createButton(".")
-        b14 = self.createButton(0)
-        b15 = self.createButton("+")
-        b16 = self.createButton("-")
-        b16a = None  # place holder for button
+        # 4th row buttons
+        b16 = self.createButton(1)
+        b17 = self.createButton(2)
+        b18 = self.createButton(3)
+        b19 = self.createButton("+")
+        b20 = None  # place holder for button
 
-        b17 = self.createButton("=", None, 34)
+        # 5th row buttonw
+        b21 = self.createButton(".")
+        b22 = self.createButton(0)
+        b23 = self.createButton("<", None)
+        b24 = self.createButton("=", None)
+        b25 = None  # place holder for button
 
-        # buttons stored in list
-        buttons = [b1, b2, b3, b4, b4a, b5, b6, b7, b8, b8a, b9,
-                   b10, b11, b12, b12a, b13, b14, b15, b16, b16a, b17, ]
+        # store buttons in list
+        buttons = [b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12,
+                   b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23, b24, b25]
 
-        # intialize counter
-        count = 0
         # arrange buttons with grid manager
-        for row in range(1, 5):
+        count = 0
+        for row in range(1, 6):
             for column in range(5):
-                # button = Button(self.master, command=self.click, width)
                 if buttons[count] is not None:
                     buttons[count].grid(row=row, column=column)
                 count += 1
-        # arrange last button '=' at the bottom
-        buttons[20].grid(row=5, column=0, columnspan=5)
 
     def createButton(self, val, write=True, width=8):
-        # this function creates a button, and takes one compulsory argument, the value that should be on the button
-
+        # this function creates a button
         return Button(self.master, text=val, command=lambda:
                       self.click(val, write), width=width, font=self.myFont,
                       height=2, padx=1, pady=1,)
 
     def click(self, text, write):
         # this function handles what happens when you click a button
-        # 'write' argument if True means the value 'val' should be written on screen, if None, should not be written on screen
+        # 'write' argument if True means the value 'val' should be written on
+        # screen, if None, should not be written on screen
         if write == None:
 
             # only evaluate code when there is an equation to be evaluated
@@ -95,7 +98,10 @@ class Calculator:
                 answer = str(eval(self.equation))
                 self.clear_screen()
                 self.insert_screen(answer, newline=True)
-            elif text == u"clear":
+
+            elif text == u'\u232b':  # this needs to backspace not clear
+                self.clear_screen()
+            elif text == "clear":
                 self.clear_screen()
 
         else:
@@ -103,7 +109,6 @@ class Calculator:
             self.insert_screen(text)
 
     def clear_screen(self):
-        # to clear screen
         # set equation to empty before deleting screen
         self.equation = ""
         self.screen.configure(state="normal")
